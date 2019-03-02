e7ca6f78382a16a62370b33cab886c459ff4b46c
pradeep <pradeepvishwakarma62@gmail.com>
2019-03-02 11:32:02 +0530

    successfully load static resources

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -9,8 +9,14 @@ public class PageController {
 	
 	@RequestMapping(value= {"/","/home","/index"})
 	public ModelAndView index() {
-		ModelAndView mv=new ModelAndView("/");
+		
+		ModelAndView mv=new ModelAndView("page");
+		
 		mv.addObject("greeting","Welcome to spring web mvc");
+		
 		return mv;
 	}
+	
+	
+	
 }
